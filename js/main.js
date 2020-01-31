09f34f1bd6691f591224378f12efa5337528f1e4
21diego <morinigodiego21@gmail.com>
2020-01-31 03:24:17 -0300

    agregando funciones

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@
 }
 document.querySelectorAll('.background>div').forEach(div => div.addEventListener("click",paint));
 document.querySelectorAll("*").forEach(e => e.addEventListener("click", event => event.target.classList.add("zoomInOut")));*/
+const maxState = 100;
+const minState = 0;
+let countClick = 0
+let maxClick = 3;
+
+
+function acariciar(){
+  let tail = document.querySelector("#tail");
+  countClick++;
+  if(!tail.classList.contains("shake")){
+    tail.classList.add("shake");
+  }
+  else if(countClick >= maxClick){
+    countClick=maxClick;
+    countClick--;
+  }
+  console.log(countClick)
+
+  
+  let count = document.querySelector("#count").textContent;
+  
+}
+
+document.querySelector("#head").addEventListener("click",acariciar);
